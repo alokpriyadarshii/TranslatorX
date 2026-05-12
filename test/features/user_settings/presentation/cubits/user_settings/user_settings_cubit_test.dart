2a35383e45f8d3c0d7875fe8330ba69448f2de87
Alok Priyadarshi <alokpriyadarshi618@gmail.com>
2026-05-12 23:55:37 +0530

    Update user_settings_cubit_test.dart

--- test/features/user_settings/presentation/cubits/user_settings/user_settings_cubit_test.dart
+++ test/features/user_settings/presentation/cubits/user_settings/user_settings_cubit_test.dart
@@ -1,7 +1,6 @@
 // Package imports:
 import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mocktail/mocktail.dart';
 
 // Project imports:
 import 'package:translatorx/constants/enums.dart';
