0237b7f60e507edb6fbcb48842ea0763bf7c64ab
Alok Priyadarshi <alokpriyadarshi618@gmail.com>
2026-05-22 21:33:42 +0530

    Configure Vercel Flutter web deployment

---
+++ vercel-build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+if [ ! -d "flutter" ]; then
+  git clone https://github.com/flutter/flutter.git -b stable --depth 1
+fi
+
+export PATH="$PATH:$(pwd)/flutter/bin"
+
+flutter config --enable-web
+flutter pub get
+flutter build web --release
